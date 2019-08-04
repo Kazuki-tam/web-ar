b06d9ac109519b0e7882a12866698354f8949247
yonemoto <yonemoto@tam-tam.co.jp>
2019-08-04 20:51:41 +0900

    Fix sw

--- sw.js
+++ sw.js
@@ -6,6 +6,13 @@ var urlsToCache = [
   'iphone.gltf',
   'https://unpkg.com/@google/model-viewer/dist/model-viewer.js',
   'https://unpkg.com/@google/model-viewer/dist/model-viewer-legacy.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css',
+  'iphone_binary.bin',
+  '/iphone_images/image_0.jpg',
+  '/iphone_images/phone_16_10_metal_normal.png',
+  '/iphone_images/phone_16_10_screen_normal.png',
+  '/iphone_images/image_3.jpg',
+  '/iphone_images/phone_16_10_shell_normal.png',
   '/css/style.css'
 ];
 //Install
